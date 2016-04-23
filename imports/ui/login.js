da15a2f4edbec39d0a9af93cd3c454095ec9e252
Gabriel Courtemanche <gabriel.courtemanche@gmail.com>
2016-04-23 18:28:57 -0400

    CSS corrections

---
+++ imports/ui/login.js
@@ -0,0 +1,32 @@
+import { Template } from 'meteor/templating';
+
+import './login.html';
+
+Template.register.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.registerEmail.value;
+        var passwordVar = event.target.registerPassword.value;
+        Accounts.createUser({
+            username: emailVar,
+            password: passwordVar
+        });
+        Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+});
+
+Template.loginForm.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+});
+
+Template.dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
