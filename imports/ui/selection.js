163427611bc0345bfa35e90d49392445eaf2b872
Martin Rancourt <vonziper@gmail.com>
2016-04-23 21:59:50 -0400

    Added categories for selection

---
+++ imports/ui/selection.js
@@ -0,0 +1,99 @@
+import { Template } from 'meteor/templating';
+import { Events } from '../api/events.js';
+
+import './selection.html';
+
+Session.setDefault("chosenCategories", []);
+
+Template.selection.events({
+    'click .vignette': function(event, template) {
+        template.$('.vignette[data-id="'+this.id+'"]').toggleClass("checked");
+
+        var chosenCategories = Session.get("chosenCategories");
+        var indexOfCategorie = _.indexOf(chosenCategories, this.name)
+        if (-1 === indexOfCategorie) {
+             chosenCategories.push(this.name);
+        } else {
+            chosenCategories.splice(indexOfCategorie, 1);
+        }
+
+        Session.set("chosenCategories", chosenCategories);
+    }
+});
+
+Template.selection.helpers({
+    events() {
+        return Events.find({});
+    },
+    categories() {
+        var categories = [
+            "Activités physiques",
+            "Camps de jour",
+            "Vélo",
+            "Pêche",
+            "Sports",
+            "Festivals et fêtes populaires",
+            "Spectacles",
+            "Soupers-bénéfice",
+            "Ateliers"
+        ];
+        return categories
+    },
+    categories_chunks() {
+        var categories = [
+            {
+                chunk: [
+                    {
+                        id: 1,
+                        name: "Activités physiques"
+                    },
+                    {
+                        id: 2,
+                        name: "Camps de jour"
+                    },
+                    {
+                        id: 3,
+                        name: "Vélo"
+                    }
+                ]
+            },
+            {
+                chunk: [
+                    {
+                        id: 4,
+                        name: "Pêche"
+                    },
+                    {
+                        id: 5,
+                        name: "Festivals et fêtes populaires"
+                    },
+                    {
+                        id: 6,
+                        name: "Spectacles"
+                    }
+                ]
+            },
+            {
+                chunk: [
+                    {
+                        id: 7,
+                        name: "Expositions et salons"
+                    },
+                    {
+                        id: 8,
+                        name: "Sports"
+                    },
+                    {
+                        id: 9,
+                        name: "Ateliers"
+                    }
+                ]
+            }
+
+        ];
+        return categories
+    },
+    chosenCategories: function () {
+        return Session.get("chosenCategories");
+    }
+});
